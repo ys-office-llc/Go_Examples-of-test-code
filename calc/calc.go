cb61b3f6c8f7820319eac10f04584cb9dbe612c5
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-04-09 11:23:29 +0900

    コメント追加

--- calc/calc.go
+++ calc/calc.go
@@ -1,5 +1,8 @@
+// Package calc provides basic arithmetic operations.
 package calc
 
+// Max returns the maximum of two integers x and y.
+// If x and y are equal, x is returned.
 func Max(x, y int) int {
 	if x < y {
 		return y
