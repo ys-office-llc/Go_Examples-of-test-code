0d419d4897e24c7af6eaf2331a9da349ee208b10
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-04-09 10:57:43 +0900

    テーブルテスト実装

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -6,27 +6,24 @@ import (
 	"github.com/ys-office-llc/Go_Examples-of-test-code/calc"
 )
 
-// サブテスト
-func TestMaxSubtests(t *testing.T) {
-	t.Run("正の数", func(t *testing.T) {
-		got := calc.Max(1, 2)
-		want := 2
-		if got != want {
-			t.Errorf("Max(1, 2) == %d, want %d", got, want)
-		}
-	})
-	t.Run("負の数", func(t *testing.T) {
-		got := calc.Max(-1, -2)
-		want := -1
-		if got != want {
-			t.Errorf("Max(-1, -2) == %d, want %d", got, want)
-		}
-	})
-	t.Run("両方", func(t *testing.T) {
-		got := calc.Max(1, -2)
-		want := 1
-		if got != want {
-			t.Errorf("Max(1, -2) == %d, want %d", got, want)
-		}
-	})
+// テーブルテスト
+func TestMaxTableDrivenTests(t *testing.T) {
+	tests := []struct {
+		name string
+		x    int
+		y    int
+		want int
+	}{
+		{"正の数", 1, 2, 2},
+		{"負の数", -1, -2, -1},
+		{"両方", 1, -2, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := calc.Max(tt.x, tt.y)
+			if got != tt.want {
+				t.Errorf("Max(%d, %d) == %d, want %d", tt.x, tt.y, got, tt.want)
+			}
+		})
+	}
 }
